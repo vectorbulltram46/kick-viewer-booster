fe72a87ebd3425260710a1e3172d393213f3c91d
vectorbulltram46 <silentinfernocrimso499@gmail.com>
2026-06-13 02:13:59 -0500

    feat: add kick-viewer-booster/src/__init__.py

---
+++ kick-viewer-booster/src/__init__.py
@@ -0,0 +1,6 @@
+from .viewer_bot import ViewerBot
+from .proxy_manager import ProxyManager
+from .user_agent_rotator import UserAgentRotator
+
+__all__ = ["ViewerBot", "ProxyManager", "UserAgentRotator"]
+__version__ = "1.0.0"
